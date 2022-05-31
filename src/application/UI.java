142107b6a335615d5b413d67e96e6d229374bf0d
Gabriel Costa <gacosil0204@gmail.com>
2022-05-31 16:22:58 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,9 +56,15 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("\nTurn : " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
@@ -105,7 +111,7 @@ public class UI {
 				.collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
 				.collect(Collectors.toList());
-		
+
 		System.out.println("\nCaptured pieces: ");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
